81dfa0b8cb598688ce2c9acd6b1a969d2fc76a50
bhivert <bhivert@e1r12p14.42.fr>
2015-01-01 23:21:04 +0100

    up makegen.sh

--- makegen.sh
+++ makegen.sh
@@ -11,11 +11,6 @@
 #                                                                              #
 # **************************************************************************** #
 
-function _usage {
-	echo "Usage: ./makegen [-s srcs_dir] [-I incs_dir] [] output_name"
-	exit
-}
-
 HEADER=\
 "# **************************************************************************** #
 #                                                                              #
@@ -33,8 +28,39 @@ CONFIG=\
 ".PHONY: all, clean, fclean, re, _mkdir
 .SUFFIXES: ";
 
-MKDIR=\
-"";
+function _usage {
+	echo "Usage: ./makegen [-L] [-s srcs_dir] [-I incs_dir] output_name"
+	exit -1
+}
+
+args=`getopt :Ls:I: $0`
+if [ $? -ne 0 ] ; then
+	_usage
+fi
+set -- $args
+
+while [-n "$*"]
+do
+	case $1 in
+		-L)
+			shift
+			;;
+		-s)
+			shift
+			shift
+			;;
+		-I)
+			shift
+			shift
+			;;
+		-:)
+			_usage
+			;;
+	esac
+done
+if [ -z "$1" ] ; then
+	_usage
+fi
 
 MAKEFILE="$HEADER\n";
 MAKEFILE+="$CONFIG\n";
