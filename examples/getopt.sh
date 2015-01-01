4fc9c52e556693e948929fa1cfab35b3617c6813
bhivert <bhivert@e1r12p14.42.fr>
2015-01-01 23:04:44 +0100

    add example getopt

---
+++ examples/getopt.sh
@@ -0,0 +1,41 @@
+#! /bin/sh
+
+function _usage {
+	echo "Usage: [-a] [-b arg] [arg ...]"
+	exit -1
+}
+
+OSTRING=":ab:"
+ARGS=`getopt "$OSTRING" $*`
+if [ $? -ne 0 ] ; then
+	_usage
+fi
+set -- $ARGS
+
+while [ -n "$*" ]
+do
+	case $1 in
+		-a)
+			echo "opt a found"
+			shift
+			;;
+		-b)
+			echo "opt b found arg: $2"
+			shift
+			shift
+			;;
+		--)
+			shift
+			break
+			;;
+		-:)
+			_usage
+			;;
+	esac
+done
+
+if [ -n "$*" ] ; then
+	echo "args less: $*";
+else
+	echo "No more args.";
+fi
