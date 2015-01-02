153b17ab9b0b84cd95af1fe3d4fa0cbce8d387a0
bhivert <hivert.benoit@gmail.com>
2015-01-02 16:07:20 +0100

    up

--- makegen.sh
+++ makegen.sh
@@ -25,30 +25,28 @@ HEADER=\
 # **************************************************************************** #";
 
 CONFIG=\
-".PHONY: all, clean, fclean, re, _mkdir
-.SUFFIXES: ";
+".PHONY: all, clean, fclean, re, _mkdir, _make
+.SUFFIXES:";
 
 _usage () {
-	echo "Usage: ./makegen [-L] [-s srcs_dir] [-I incs_dir] NAME"
+	echo "Usage: $0 [-L] [-C dir [depends]] NAME"
 }
 
-args=`getopt :Ls:I: $*`
+args=`getopt LC: $*`
 if [ $? -ne 0 ] ; then
 	_usage
 	exit 1
 fi
 set -- $args
-
+IS_LIB=0
 while [ -n "$*" ]
 do
 	case $1 in
 		-L)
+			IS_LIB=1
 			shift
 			;;
-		-s)
-			shift 2
-			;;
-		-I)
+		-C)
 			shift 2
 			;;
 		--)
@@ -61,7 +59,20 @@ if [ -z "$1" ] ; then
 	_usage
 	exit 2
 fi
+
 NAME="NAME\t:\t$1"
+
+
+SRCS="
+ifeq (\$(DEBUG), 0)
+	CC			=	gcc
+	CFLAGS		=	-Wall -Wextra \$(INCS_FLAGS) -O3
+else
+	CC			=	cc
+	CFLAGS		=	-Wall -Wextra -Werror \$(INCS_FLAGS) -g3
+endif"
+
+
 
 MAKEFILE="$HEADER\n";
 MAKEFILE="$MAKEFILE$CONFIG\n";
