24676630d78b420ddd04b37b8597f13f920621f9
bhivert <hivert.benoit@gmail.com>
2015-03-25 05:36:43 +0100

    up genclass

--- genclass/genclass.sh
+++ genclass/genclass.sh
@@ -3,9 +3,10 @@
 if [ -e "$1".Class.hpp ] ; then
 	echo "Class exist already."
 else
-	sed 's|name|'$1'|g' NAME.Class.cpp > "$1".Class.cpp
-	sed 's|name|'$1'|g' NAME.Class.hpp > "$1".Class.hpp.tmp
-	PROTECT=`echo "$1" | tr '[:lower:]' '[:upper:]'`
-	sed 's|NAME|'$PROTECT'|g' "$1".Class.hpp.tmp > "$1".Class.hpp
+	path="$HOME/00-Dev/sh_script/genclass/"
+	sed 's|name|'$1'|g' "$path"NAME.Class.cpp > "$1".Class.cpp
+	sed 's|name|'$1'|g' "$path"NAME.Class.hpp > "$1".Class.hpp.tmp
+	protect=`echo "$1" | tr '[:lower:]' '[:upper:]'`
+	sed 's|NAME|'$protect'|g' "$1".Class.hpp.tmp > "$1".Class.hpp
 	rm "$1".Class.hpp.tmp
 fi
